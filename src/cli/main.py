95aacb810bf216591369f2d35f17ab479a0ca2f3
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-05-24 19:09:57 +0200

    fix linting

--- src/cli/main.py
+++ src/cli/main.py
@@ -38,8 +38,8 @@ def main(lang, personality, form_url, enforce_quality_check):
 
     reset_screenshot_dir()
 
-    # questions = extract_google_form(form_url)
-    # print(questions)
+    questions = extract_google_form(form_url)
+    print(questions)
 
     question = "Co motywuje Cię do pracy?"
     traits = ["casual", "honest"]
